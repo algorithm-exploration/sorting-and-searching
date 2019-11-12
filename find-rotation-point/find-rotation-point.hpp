baecd8e1864f65a306f0c3700d807dbe82bfa533
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-12 16:05:13 -0600

    Create find-rotation-point.hpp

---
+++ find-rotation-point/find-rotation-point.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+size_t findRotationPoint(const vector<string>& words)
+{
+    // find the rotation point in the array
+    
+
+    return 0;
+}
