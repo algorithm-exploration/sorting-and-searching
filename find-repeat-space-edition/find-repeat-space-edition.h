3c6a8a116d76b4da6b19cefd939b5d869ebb91a6
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-12 16:08:49 -0600

    Create find-repeat-space-edition.h

---
+++ find-repeat-space-edition/find-repeat-space-edition.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+unsigned int findRepeat(const unsigned int *numbers, size_t length)
+{
+    // find a number that appears more than once
+    
+
+    return 0;
+}
