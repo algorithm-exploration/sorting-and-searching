2eef593c967b320a522a596814f05dc67387a7ef
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-12 16:07:51 -0600

    Create find-repeat-space-edition.hpp

---
+++ find-repeat-space-edition/find-repeat-space-edition.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+unsigned int findRepeat(const vector<unsigned int>& numbers)
+{
+    // find a number that appears more than once
+    
+
+    return 0;
+}
