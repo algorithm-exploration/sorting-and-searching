675b9efc053713faff8bf8801cb0564ba1a5ba4b
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-12 16:04:09 -0600

    Create find-rotation-point.h

---
+++ find-rotation-point/find-rotation-point.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+size_t findRotationPoint(const char **words, size_t length)
+{
+    // find the rotation point in the array
+    
+
+    return 0;
+}
